d77e9b7525b81671734bdf5700c0235af4becd1e
Alex Grek <alexgrek.hq@gmail.com>
2016-02-22 20:46:16 +0200

    added FileBuffer class

---
+++ Translator/FileBuffer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace Translator
+{
+    class FileBuffer: IDisposable
+    {
+        FileStream _stream;
+        byte _current;
+
+        public FileBuffer(string path)
+        {
+            _stream = new FileStream(path, FileMode.Open);
+        }
+
+        public byte CurrentByte {
+            get
+            {
+                return _current;
+            }
+        }
+
+        public char CurrentChar
+        {
+            get
+            {
+                return (char)_current;
+            }
+        }
+
+        public bool TryMoveNext()
+        {
+            if (_stream.Position >= _stream.Length)
+                return false;
+
+            _current = (byte)_stream.ReadByte();
+
+            return true;
+        }
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+    }
+}
--- Translator/Program.cs
+++ Translator/Program.cs
@@ -11,7 +11,7 @@ namespace Translator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
-
+            
         }
     }
 }
